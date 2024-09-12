ad9bd9c1eec019b98ba43b2392174c56455c61e2
Kleber <kleber@gmail.com>
2024-09-12 16:16:34 -0300

    realizado login estatico

---
+++ test/modelo/Pessoa.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+public class Pessoa {
+ private String nome;
+ private String sobrenome;
+ 
+ 
+}
